0fd42c15f6d7e1c89e7378e25c0e3db615b287c2
Aarush Pal <74675248+aarushpal@users.noreply.github.com>
2022-11-09 09:28:44 +0530

    added project files

---
+++ client.c
@@ -0,0 +1,485 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+
+// server.smtp.com is the domain name of the server
+// 25 is the port number of the server
+#define domain "server.smtp.com"
+char mail_from[100];
+char mail_to[100];  
+
+
+// Function to send image to server
+void send_image(int socket)
+{
+    FILE *fpimage;
+	int size;
+	int readsize;
+	int status;
+	int packet_index = 1;
+	char send_buffer[10239];
+	char read_buffer[256];
+    char f_name[100];
+
+    printf("\nEnter Image Path/Image file name : ");
+    scanf("%s",f_name);
+
+    fpimage = fopen(f_name, "r");
+	printf("\nCLIENT: \nGetting Image Size...\n");  
+	
+    if(fpimage == NULL){
+		printf("Error occured while opening Image File!\n");
+	}
+	
+    fseek(fpimage, 0, SEEK_END);
+	size = ftell(fpimage);
+	fseek(fpimage, 0, SEEK_SET);
+	printf("Total Image size: %i\n",size);
+    
+	printf("Sending Image Size\n");
+	write(socket, (void *)&size, sizeof(int));
+
+	printf("Sending Image as a byte array...");
+
+    do{ 
+	    status=read(socket, &read_buffer , 255);
+
+	}while(status < 0);
+
+    //feof is a macro that returns true if the end of file has been reached
+    while(!feof(fpimage))
+    {
+        readsize = fread(send_buffer, 1, sizeof(send_buffer)-1, fpimage);
+
+	    do{
+		    status = write(socket, send_buffer, readsize);  
+	    }while(status<0);
+  
+	    printf(" \n");
+	    printf(" \n");
+
+        packet_index++;
+        bzero(send_buffer, sizeof(send_buffer));
+    }
+}
+
+
+char * Mail_Header(char *from,char *to,char *sub,char *content)
+{
+    // time_t is arithmetic time type
+    // time() returns the current time of the system as a time_t value
+    time_t t;
+    time(&t); 
+
+    char *header = NULL;
+    char date[26];
+    char DATE_h[8 + strlen(date)];
+    char sender[8 + strlen(from)];   //FROM: sender's_email\r\n
+    char recep[6 + strlen(to)];      //TO: recepient's_email\r\n
+    char subject[11 + strlen(sub)];
+    char content_a[1 + strlen(content) + 2 + 1 + 1];
+
+    //Date
+
+    /*localtime converts a time_t value to calendar time and returns a pointer to a 
+    tm structure with its members filled with the corresponding values*/
+    //strftime converts the broken-down time, expressed as local time, to a string
+    // sprintf is used to write formatted output to a string
+
+    strftime(date, (33), "%a %d %b %Y %H:%M:%S",localtime(&t));
+    sprintf(DATE_h,"DATE: %s\r\n",date);
+    sprintf(sender,"FROM: %s\r\n",from);
+    sprintf(subject,"Subject: %s\r\n",sub);
+    sprintf(recep,"TO: %s\r\n",to);
+    //extra \n is used to end the header part
+
+    sprintf(content_a,"%s\r\n",content);
+
+    int header_length = strlen(DATE_h) + strlen(sender) + strlen(subject) + strlen(recep) + strlen(content_a);
+
+    header = (char *) malloc(header_length * sizeof(char));
+
+    //copying all the header parts to the header variable
+
+    //memcpy copies n bytes from memory area src to memory area dest
+    memcpy(&header[0], &DATE_h, strlen(DATE_h));
+    memcpy(&header[0 + strlen(DATE_h)], &sender , strlen(sender));
+    memcpy(&header[0 + strlen(DATE_h) + strlen(sender)], &subject , strlen(subject));
+    memcpy(&header[0 + strlen(DATE_h) + strlen(sender) + strlen(subject)], &recep , strlen(recep));
+    memcpy(&header[0 + strlen(DATE_h) + strlen(sender) + strlen(subject) + strlen(recep)], &content_a , strlen(content_a));
+    
+    return header;
+}
+
+
+int main(int argc, char *argv[])
+{
+    //struct hostent *host is a structure that will get information about the host
+    //struct sockaddr_in server_addr is a structure that will contain an internet address
+    int socket_id,n;
+    int portno;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char cname[256];
+    char buff[10240];
+
+    if(argc < 3)
+    {
+        perror("\nPlease enter the hostname and port number.\n");
+        exit(0);
+    }
+    portno = atoi(argv[2]);
+
+    socket_id = socket(AF_INET,SOCK_STREAM, 0);
+
+    if(socket_id < 0)
+    {
+        perror("\nError occured while opening the socket!\n");
+        exit(0);
+    }
+    server = gethostbyname(argv[1]);
+
+    if(server == NULL)
+    {
+        perror("\nError: No such host found!\n");
+        exit(0);
+    }
+
+    // Initialize socket structure
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr,server->h_length);
+    serv_addr.sin_port = htons(portno);
+    
+    //connect to the server
+    int connect_id;
+    connect_id = connect(socket_id,(struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if(connect_id < 0)
+    {
+        perror("Error occured while connecting to server...\n");
+    }
+       
+    //Implementing commands which are used b/w Client and SMTP Server for communication
+    do
+    {
+        // Taking command input from user
+        printf("Enter the command : ");
+        intake: gets(cname);
+        //cname[strlen(cname) + 1] = '\0';
+        
+        char code[4];       //to store the 3 digit response code received from server
+        
+        // HELO command
+        // This command is used to identify the client to the server
+        // It is the first command that a client sends after establishing a TCP connection
+        // It is used to start the SMTP conversation
+        if(strcasecmp(cname,"HELO") == 0)
+        {
+            bzero(buff,10240);
+            strcpy(buff,"HELO ");
+            strcat(buff, domain);
+            strcat(buff,"\r\n");
+            n = write(socket_id,buff,strlen(buff));
+            if(n < 0)
+            {
+                printf("\nError occured while writing to socket!\n");
+            }
+            printf("\nCLIENT : %s",buff);  //HELO domain
+            bzero(buff,10240);
+            n = read(socket_id,buff,10239);
+            if(n < 0)
+            {
+                printf("\nError occured while reading from socket!\n");
+            }
+            printf("SERVER : %s\n",buff);    //250 Hello domain
+
+            //checking error
+            code[0] = buff[0];
+            code[1] = buff[1];
+            code[2] = buff[2];
+            code[3] = '\0';
+
+            if(strcmp(code,"250") == 0)
+            {
+                printf("\nGo to next command...\n\n");
+            }
+            else{
+                printf("\nError occured!\n\n");
+            }
+            // fflush is used to clear the output buffer and move its content to console
+            fflush(stdin);
+        }
+        
+
+        // MAIL FROM command
+        // MAIL FROM: <sender's email address>
+        else if(strcasecmp(cname,"MAIL FROM") == 0)
+        {
+            bzero(buff,10240);
+            printf("\nEnter Sender Email id : ");
+            scanf("%s",mail_from);
+            strcpy(buff,"MAIL FROM:<");
+            strcat(buff,mail_from);
+            strcat(buff,">");
+            strcat(buff,"\r\n");
+            n = write(socket_id,buff,strlen(buff));
+            if(n < 0)
+            {
+                printf("\nError occured while writing to socket!\n");
+            }
+            printf("\nCLIENT : %s",buff);  //MAIL FROM:<your email id>
+            bzero(buff,10240);
+            n = read(socket_id,buff,10239);
+            if(n < 0)
+            {
+                printf("\nError occured while reading from socket!\n");
+            }
+            printf("SERVER : %s\n",buff);  //250 OK
+
+            //checking error
+            code[0] = buff[0];
+            code[1] = buff[1];
+            code[2] = buff[2];
+            code[3] = '\0';
+
+            if(strcmp(code,"250") == 0)
+            {
+                printf("\nGo to next command...\n\n");
+            }
+            else{
+                printf("\nError occured!\n\n");
+            }
+            fflush(stdin);
+        }
+
+
+        // RCTP TO command
+        // this command is used to send the email to the receiver
+
+        //strcasecmp is used to compare two strings without considering the case
+        else if(strcasecmp(cname,"RCPT TO") == 0)
+        {
+            bzero(buff,10240);
+            printf("\nEnter Recipient Email id : ");
+            scanf("%s",mail_to);
+            strcpy(buff,"RCPT TO:<");
+            strcat(buff,mail_to);
+            strcat(buff,">");
+            strcat(buff,"\r\n");
+            n = write(socket_id,buff,strlen(buff));
+            if(n < 0)
+            {
+                printf("\nError occured while writing to socket!\n");
+            }
+            printf("\nCLIENT : %s",buff);  //RCPT TO:<your email id>
+            bzero(buff,10240);
+            n = read(socket_id,buff,10239);
+            if(n < 0)
+            {
+                printf("\nError occured while reading from socket!\n");
+            }
+            printf("SERVER : %s\n",buff);  //250 OK
+
+            //checking error
+            code[0] = buff[0];
+            code[1] = buff[1];
+            code[2] = buff[2];
+            code[3] = '\0';
+
+            if(strcmp(code,"250") == 0)
+            {
+                printf("\nGo to next command...\n\n");
+            }
+            else{
+                printf("\nError occured!\n\n");
+            }
+            fflush(stdin);
+        }
+
+        // DATA command
+        // This command is used to send the actual message to the receiver
+        else if(strcasecmp(cname,"DATA") == 0)          
+        {
+            bzero(buff,10240);
+            strcpy(buff,"DATA");
+            strcat(buff,"\r\n");
+            n = write(socket_id,buff,strlen(buff));
+            if(n < 0)
+            {
+                printf("\nError occured while writing to socket!\n");
+            }
+            printf("\nCLIENT : %s",buff);  //DATA
+            bzero(buff,10240);
+            n = read(socket_id,buff,10239);
+            if(n < 0)
+            {
+                printf("\nError occured while reading from socket!\n");
+            }
+            printf("SERVER : %s\n",buff);  //354 Send message content; end with <CRLF>.<CRLF>
+
+            //checking error
+            code[0] = buff[0];
+            code[1] = buff[1];
+            code[2] = buff[2];
+            code[3] = '\0';
+
+            if(strcmp(code,"354") == 0)
+            {
+                printf("\nReady to send header data!\n\n");
+            }
+            else{
+                printf("\nError occured!\n\n");
+            }
+
+            //creating a mail header
+            char sub[150];
+            char content[450];
+            printf("\nEnter Subject : ");
+            scanf("%[^\n]",sub);
+            
+            printf("\nEnter content : (Press Tab and Enter Key to end)\n");
+            scanf("%[^\t]",content);
+
+            bzero(buff,10240);
+            //Mail_header function declared above
+            strcpy(buff, Mail_Header(mail_from,mail_to,sub,content));  //assigning header to buffer
+            
+            n = write(socket_id,buff,strlen(buff));                     
+            if(n < 0)
+            {
+                printf("\nError occured while writing to socket!\n");
+            }
+            printf("\nCLIENT : ====| Mail header & content |====\n%s\n",buff);  //header content
+            
+            bzero(buff,10240);
+            strcpy(buff,".\r\n");
+            n = write(socket_id,buff,strlen(buff));                    
+            if(n < 0)
+            {
+                printf("\nError occured while writing to socket!\n");
+            }
+            
+            bzero(buff,10240);
+            n = read(socket_id,buff,10239);  
+            if(n < 0)
+            {
+                printf("\nError occured while reading from socket!\n");
+            } 
+            printf("SERVER : %s\n",buff);     //250 OK
+
+            //checking error
+            code[0] = buff[0];
+            code[1] = buff[1];
+            code[2] = buff[2];
+            code[3] = '\0';
+
+            if(strcmp(code,"250") == 0)
+            {	
+                printf("\nGo to next command...\n\n");
+            }
+            else{
+                printf("\nError occured!\n\n");
+            }
+            fflush(stdin);                                  
+        }
+
+
+
+        // ATTACHMENT command
+        // This command is used to send the attachment to the receiver
+        else if(strcasecmp(cname,"ATTACHMENT") == 0){
+            bzero(buff,10240);
+			strcpy(buff,"Attachment");
+			strcat(buff,"\r\n");
+			n = write(socket_id,buff,strlen(buff));
+			if(n < 0)
+			{
+			    printf("\nError occured while writing to socket!\n");
+			}
+			printf("\nCLIENT : %s",buff);  //DATA
+			bzero(buff,10240);
+			n = read(socket_id,buff,10239);
+			if(n < 0)
+			{
+			    printf("\nError occured while reading from socket!\n");
+			}
+			printf("SERVER : %s\n",buff);  //420 send Attachment
+
+			code[0] = buff[0];
+			code[1] = buff[1];
+			code[2] = buff[2];
+			code[3] = '\0';
+
+			if(strcmp(code,"420") == 0)
+			{
+			    printf("\nReady to send image!\n\n");
+			}
+			else{
+			    printf("\nError occured!\n\n");
+			}
+
+
+			send_image(socket_id);
+			bzero(buff,10240);
+			n = read(socket_id,buff,10239);
+			if(n < 0)
+			{
+			    printf("\nError occured while reading from socket!\n");
+			}
+			printf("SERVER : %s\n\n",buff);
+        }
+
+
+
+        // QUIT command
+        // This command is used to close the connection with the server
+        else if(strcasecmp(cname,"QUIT") == 0)
+        {
+            bzero(buff,10240);
+            strcpy(buff,"QUIT");
+            strcat(buff,"\r\n");
+            n = write(socket_id,buff,strlen(buff));
+            if(n < 0)
+            {
+                printf("\nError occured while writing to socket!\n");
+            }
+            printf("\nCLIENT : %s",buff);  //QUIT
+            bzero(buff,10240);
+            n = read(socket_id,buff,10239);
+            if(n < 0)
+            {
+                printf("\nError occured while reading from socket!\n");
+            }
+            printf("SERVER : %s\n",buff);    //221 Bye
+
+            //checking error
+            code[0] = buff[0];
+            code[1] = buff[1];
+            code[2] = buff[2];
+            code[3] = '\0';
+
+            if(strcmp(code,"221") == 0)
+            {
+                printf("\nConnection closed successfully with SMTP Server!\n\n");
+            }
+            else{
+                printf("\nError occured!\n\n");
+            }
+            fflush(stdin);
+        }
+        else
+        {	
+        	strcpy(cname,"");
+        	goto intake;
+        }
+        
+    }while(strcmp(cname,"QUIT") != 0);   
+}
+
